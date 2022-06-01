7684cec4e960bb1a811275ab8f6c5a66b247854d
Larissa Fernanda <larifernand7@gmail.com>
2022-06-01 21:54:33 +0044

    a

---
+++ src/components/PaletaLista/PaletaLista.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import PaletaListaItem from "components/PaletaListaItem/PaletaListaItem";
+import "./PaletaLista.css";
+import { paletas } from "mocks/paletas.js";
+
+function PaletaLista() {
+  const [paletaSelecionada, setPaletaSelecionada] = useState({});
+
+  const adicionarItem = (paletaIndex) => {
+    const paleta = {
+      [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) + 1,
+    };
+    setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
+  };
+
+  const removerItem = (paletaIndex) => {
+    const paleta = {
+      [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) - 1,
+    };
+    setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
+  };
+
+  return <div className="PaletaLista">
+    {paletas.map((paleta, index) => (
+	<PaletaListaItem
+		key={`PaletaListaItem-${index}`}
+		paleta={paleta}
+		quantidadeSelecionada={paletaSelecionada[index]}
+		index={index} 
+    onAdd={index => adicionarItem(index)}
+			onRemove={index => removerItem(index)} />
+	))}
+  </div>
+}
+export default PaletaLista;
